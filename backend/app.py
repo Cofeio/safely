91efc688b8dc6e794ffcd97e17fb2b768fcb8734
Kishan <phi.liney@gmail.com>
2019-02-24 02:07:33 -0800

    500

--- backend/app.py
+++ backend/app.py
@@ -24,7 +24,7 @@ def check_here_api(latitude, longitude):
         'app_code': f'{APP_CODE}',
         'layer_ids': 1,
         'key_attribute': 'NAME',
-        'proximity': f'{latitude},{longitude}'
+        'proximity': f'{latitude},{longitude},500'
     }
 
     response = requests.get('https://gfe.api.here.com/2/search/proximity.json',
