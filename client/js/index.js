ef8a3aa688e564f30673efa9a9a2be535b7f08ca
Jay Rezvani <jarezvani@gmail.com>
2019-02-23 18:15:26 -0800

    create client and public folders

---
+++ client/js/index.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+
+ const div = (
+    <div>
+      <h1>ProtEcT yO kiDs!</h1>
+    </div>
+  )
+
+ ReactDOM.render(div, document.getElementById('root'))
---
+++ client/webpack.config.js
@@ -0,0 +1,33 @@
+const webpack = require('webpack')
+const path = require('path')
+
+const BUILD_DIR = path.resolve(__dirname, 'dist')
+const APP_DIR = path.resolve(__dirname, 'js')
+const LESS_DIR = path.resolve(__dirname, 'less')
+
+
+const entry_points = {
+  index: `${APP_DIR}/index.js`
+}
+
+ 
+module.exports = {
+  entry: entry_points,
+  output: {
+    publicPath: '/dist/',
+    path: BUILD_DIR,
+    filename: 'js/[name].js',
+    libraryTarget: 'umd'
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: [
+          'babel-loader',
+        ],
+      },
+    ],
+  },
+}
