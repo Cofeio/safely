86e8336e94a225a6b5843cb81d3fc136334152b0
Kishan <phi.liney@gmail.com>
2019-02-24 12:38:56 -0800

    copypasta

--- client/js/child.js
+++ client/js/child.js
@@ -22,6 +22,60 @@ export default class Child extends Component {
     this.sendLocationUpdate = this.sendLocationUpdate.bind(this)
   }
 
+  /**
+   * Calculates and displays a walking route from the St Paul's Cathedral in London
+   * to the Tate Modern on the south bank of the River Thames
+   *
+   * A full list of available request parameters can be found in the Routing API documentation.
+   * see:  http://developer.here.com/rest-apis/documentation/routing/topics/resource-calculate-route.html
+   *
+   * @param   {H.service.Platform} platform    A stub class to access HERE services
+   */
+  calculateRouteFromAtoB (currentLocation, endLocation) {
+    var router = platform.getRoutingService(),
+      routeRequestParams = {
+        mode: 'balanced;pedestrian',
+        representation: 'display',
+        waypoint0: `${currentLocation.lat},${currentLocation.lon}`, // St Paul's Cathedral
+        waypoint1: `${endLocation.lat},${endLocation.lon}`,  // Tate Modern
+        routeattributes: 'waypoints,summary,shape,legs',
+        maneuverattributes: 'direction,action'
+      };
+
+
+    router.calculateRoute(
+      routeRequestParams,
+      onSuccess,
+      onError
+    );
+  }
+  /**
+   * This function will be called once the Routing REST API provides a response
+   * @param  {Object} result          A JSONP object representing the calculated route
+   *
+   * see: http://developer.here.com/rest-apis/documentation/routing/topics/resource-type-calculate-route.html
+   */
+  onSuccess(result) {
+    var route = result.response.route[0];
+   /*
+    * The styling of the route response on the map is entirely under the developer's control.
+    * A representitive styling can be found the full JS + HTML code of this example
+    * in the functions below:
+    */
+    addRouteShapeToMap(route);
+    addManueversToMap(route);
+
+    addWaypointsToPanel(route.waypoint);
+    addManueversToPanel(route);
+    addSummaryToPanel(route.summary);
+    // ... etc.
+  }
+
+
+  onError(error) {
+    alert('Ooops!');
+  }
+
   componentDidMount(){
     this.api.getChildLocation().then(({data, message}) => {
       this.updateLocation(data)
